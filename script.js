7dda1ae41a61f2dab37168bb7f0240d573d7c492
Kendry On Sang <kendrypaiba15@gmail.com>
2025-06-24 15:37:07 -0600

    updating to english

--- script.js
+++ script.js
@@ -1,31 +1,14 @@
-//Función que me aplica el estilo a la opciòn seleccionada y quita la previamente seleccionada
-function seleccionar(link) {
-    var opciones = document.querySelectorAll('#links  a');
-    opciones[0].className = "";
-    opciones[1].className = "";
-    opciones[2].className = "";
-    opciones[3].className = "";
-    opciones[4].className = "";
-    link.className = "seleccionado";
+var check = document.querySelector(".check");
+check.addEventListener('click', language);
 
-    //Hacemos desaparecer el menu una vez que se ha seleccionado una opcion
-    //en modo responsive
-    var x = document.getElementById("nav");
-    x.className = "";
-}
-
-//función que muestra el menu responsive
-function responsiveMenu() {
-    var x = document.getElementById("nav");
-    if (x.className === "") {
-        x.className = "responsive";
-    } else {
-        x.className = "";
+function language() {
+    let id=check.checked;
+    if (id == true) {
+        location.href = "img/index.html";
+    }else {
+        location.href = "../index.html";
     }
 }
-
-//detecto el scrolling para aplicar la animación del la barra de habilidades
-window.onscroll = function() { efectoHabilidades() };
 
 //funcion que aplica la animación de la barra de habilidades
 function efectoHabilidades() {
